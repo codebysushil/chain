106988cf08b752a46e4d9f0c999f49854ac77d45
Sushil Kumar 🍀 <sushilkumbhar77@gmail.com>
2025-12-23 17:20:32 +0530

    Add BDQuery class with select and where methods

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<?php
+
+class BDQuery
+{
+  public function __construct()
+  {
+    // const
+  }
+
+  public function select()
+  {
+    return $this;
+  }
+  public function where()
+  {
+    return $this;
+  }
+}
